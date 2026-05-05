925ac482f056ee3e59625df47b462725bbde5b28
salva <salvaarancegallego@gmail.com>
2026-05-05 09:46:01 +0200

    Borrar lista de ciclos de ciclo

--- Entidades/Ciclo.vb
+++ Entidades/Ciclo.vb
@@ -1,7 +1,5 @@
 ﻿Public Class Ciclo
 
-    Private Ciclos As New List(Of Ciclo)
-
     Public Property CodigoCiclo As String
     Public Property NombreCiclo As String
     Public Sub New(codigoCiclo As String, nombreCiclo As String)
